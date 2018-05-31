65582d81d30f2078ea1d50d7075b20c6fece3011
Unknown <johnn_lds@hotmail.com>
2018-05-31 15:19:57 -0300

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_initialize :set_default_role, :if => :new_record?
+  enum role: [:user, :admin]
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
