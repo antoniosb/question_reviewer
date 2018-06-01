c52bf621d8bb93ae86c68de87bce2edec7d82971
Unknown <johnn_lds@hotmail.com>
2018-06-01 09:15:49 -0300

    add status enum

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :options
+  accepts_nested_attributes_for :options,
+                                :allow_destroy => true,
+                                :reject_if     => :all_blank
+  enum status: [:pending, :approved, :rejected]
 end
