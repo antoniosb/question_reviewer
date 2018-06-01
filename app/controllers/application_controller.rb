0ec45fd76e249f83125e2856eaf1f393064ec17c
Unknown <johnn_lds@hotmail.com>
2018-05-31 22:41:03 -0300

    define default redirect for each role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    admin_questions_path if current_user.admin?
+    questions_path
+  end
 end
