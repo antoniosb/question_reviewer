ed9e6519ad4079fa84d5b8663eb8223e9d5ff413
Unknown <johnn_lds@hotmail.com>
2018-06-01 09:17:10 -0300

    add on delete cascade

--- db/migrate/20180531185912_create_options.rb
+++ db/migrate/20180531185912_create_options.rb
@@ -3,7 +3,7 @@ class CreateOptions < ActiveRecord::Migration[5.1]
     create_table :options do |t|
       t.string :content
       t.boolean :answer
-      t.references :question, foreign_key: true
+      t.references :question, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
