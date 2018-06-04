095d3ba9d206874134b911614b7a764c35883c51
Unknown <johnn_lds@hotmail.com>
2018-06-04 00:51:02 -0300

    model for admin reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180601014504) do
+ActiveRecord::Schema.define(version: 20180604013229) do
 
   create_table "options", force: :cascade do |t|
     t.string "content"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20180601014504) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_reviews_on_question_id"
   end
 
   create_table "users", force: :cascade do |t|
