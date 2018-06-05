9862f8fb54164e6a0bbd0c6a9af370854062489c
Unknown <johnn_lds@hotmail.com>
2018-06-05 09:33:42 -0300

    seed to create default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.all.empty?
+  user = User.new
+  user.email = 'admin@email.com'
+  user.password = '12341234'
+  user.password_confirmation = '12341234'
+  user.save!
+  user.admin!
+end
