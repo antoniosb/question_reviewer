a3a1140971c3ca31106c8c8e4129270859dad9da
Unknown <johnn_lds@hotmail.com>
2018-06-04 22:03:34 -0300

    create factories for tests

---
+++ spec/factories/options.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :option do
+    content "MyString"
+    answer false
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email "teste@gmail.com"
+    password "12341234"
+    role "user"
+  end
+end
