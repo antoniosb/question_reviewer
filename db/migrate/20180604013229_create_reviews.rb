a99973846b96303499d5b9cbba52c3e425bccbb9
Unknown <johnn_lds@hotmail.com>
2018-06-04 22:02:37 -0300

    create reviews migration

--- db/migrate/20180604013229_create_reviews.rb
+++ db/migrate/20180604013229_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
       t.string :comment
-      t.references :question, foreign_key: true
+      t.references :question, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
